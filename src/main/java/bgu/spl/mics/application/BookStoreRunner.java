0a0e443074a1261d2a01cfe1232020f3990ec26a
Aviv <aavivgai@gmail.com>
2018-12-07 13:40:06 +0200

    aviv1

--- src/main/java/bgu/spl/mics/application/BookStoreRunner.java
+++ src/main/java/bgu/spl/mics/application/BookStoreRunner.java
@@ -13,6 +13,8 @@ import org.json.simple.parser.ParseException;
 
 import java.util.HashMap;
 
+//not done
+
 
 /** This is the Main class of the application. You should parse the input file, 
  * create the different instances of the objects, and run the system.
